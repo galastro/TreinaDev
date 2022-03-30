36f5d6176b85b63c00086fc08371f3d116989a0f
Leandro <lcgalastro@gmail.com>
2022-03-30 19:45:58 -0300

    aula boas praticas

--- cookbook/cookbook.rb
+++ cookbook/cookbook.rb
@@ -1,11 +1,14 @@
+INSERIR_RECEITA = 1
+VISUALIZAR_RECEITAS = 2
+SAIR = 3
 def bem_vindo()
     puts "Bem-vindo ao Cookbook, sua rede social de receitas!"
 end
 
 def menu()
-    puts "[1] Cadastrar uma receita"
-    puts "[2] Ver todas as receitas"
-    puts "[3] Sair"
+    puts "[#{INSERIR_RECEITA}] Cadastrar uma receita"
+    puts "[#{VISUALIZAR_RECEITAS}] Ver todas as receitas"
+    puts "[#{SAIR}] Sair"
 
     print "Escolha uma opção: "
     return gets.to_i()
@@ -22,12 +25,16 @@ def inserir_receita()
     return {nome: nome, tipo: tipo}
 end
 
-def imprimir_receitas(r)
+def imprimir_receitas(receitas)
     puts "========= Receitas cadastradas ========="
-    r.each do |receita|
+    receitas.each do |receita|
         puts("#{receita[:nome]} - #{receita[:tipo]}")    
     end
     puts
+    if receitas.empty?
+        puts "Nenhuma receita cadastrada."
+        puts
+    end
 end 
 
 # -------------------¯\_(ツ)_/¯ ----------------------------
@@ -37,19 +44,19 @@ receitas = []
 
 opcao = menu()
 
-while(opcao != 3) do
-    if (opcao == 1)
+loop do
+    if (opcao == INSERIR_RECEITA)
         receitas << inserir_receita()
-    elsif(opcao == 2)
+    elsif(opcao == VISUALIZAR_RECEITAS)
         imprimir_receitas(receitas)
+    elsif(opcao == SAIR)
+        break
     else
         puts "Opção inválida!"
 
     end
     opcao = menu()
 end
-# receitas.each do |receita|
-#     puts receita
-# end
-# puts
- puts 'Obrigado por usar o Cookbook. Até logo!'
+
+puts
+puts 'Obrigado por usar o Cookbook. Até logo!'
